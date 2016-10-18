dccfd7f8199da1988de1aa76d64c078a05646f33
Mike Liu <mikelkl@foxmail.com>
2016-10-18 17:18:37 +0800

    fix <loading> element bug by v-bind:style and use element.classList to replace element.style

--- src/components/Drawer.vue
+++ src/components/Drawer.vue
@@ -1,12 +1,13 @@
 <template>
   <div>
     <div class="demo-layout mdl-layout mdl-js-layout mdl-layout--fixed-drawer mdl-layout--fixed-header">
-      <header class="demo-header mdl-layout__header mdl-color--grey-100 mdl-color-text--grey-600">
+      <header class="demo-header mdl-layout__header mdl-layout__header--waterfall mdl-color--teal-600 mdl-color-text--grey-600">
         <div class="mdl-layout__header-row">
-          <span class="mdl-layout-title">Home</span>
+          <img src="../assets/images/logo.png" width="30" height="30">
+          <span class="mdl-layout-title header-font">Keeper</span>
           <div class="mdl-layout-spacer"></div>
           <div class="mdl-textfield mdl-js-textfield mdl-textfield--expandable">
-            <label class="mdl-button mdl-js-button mdl-button--icon" for="search">
+            <label class="mdl-button mdl-js-button mdl-button--icon header-font" for="search">
               <i class="material-icons">search</i>
             </label>
             <div class="mdl-textfield__expandable-holder">
@@ -14,14 +15,9 @@
               <label class="mdl-textfield__label" for="search">Enter your query...</label>
             </div>
           </div>
-          <button class="mdl-button mdl-js-button mdl-js-ripple-effect mdl-button--icon" id="hdrbtn">
-            <i class="material-icons">more_vert</i>
-          </button>
-          <ul class="mdl-menu mdl-js-menu mdl-js-ripple-effect mdl-menu--bottom-right" for="hdrbtn">
-            <li class="mdl-menu__item">About</li>
-            <li class="mdl-menu__item">Contact</li>
-            <li class="mdl-menu__item">Legal information</li>
-          </ul>
+        </div>
+        <div class="mdl-layout__header-row">
+          <span class="header-title">{{ selectedTitle }}</span>
         </div>
       </header>
       <div v-on:click="hide"  class="demo-drawer mdl-layout__drawer mdl-color--blue-grey-900 mdl-color-text--blue-grey-50">
@@ -41,9 +37,9 @@
             </ul>
           </div>
         </header>
-        <nav class="demo-navigation mdl-navigation mdl-color--blue-grey-800">
-          <a class="mdl-navigation__link" href=""><img class="icon" src="../assets/images/icons/home.svg">首页</a>
-          <router-link to="/treatment-record" class="mdl-navigation__link"><img class="icon" src="../assets/images/icons/record.svg">就诊记录</router-link>
+        <nav v-on:click="activate" class="demo-navigation mdl-navigation mdl-color--blue-grey-800">
+          <!-- <router-link to="/home" class="mdl-navigation__link"><img class="icon" src="../assets/images/icons/home.svg">首页</router-link> -->
+          <router-link to="/treatment-record" class="mdl-navigation__link" v-bind:class="{ display: loadingDisplay }"><img class="icon" src="../assets/images/icons/record.svg">就诊记录</router-link>
           <router-link to="/ecg" class="mdl-navigation__link"><img class="icon" src="../assets/images/icons/ecg.svg">心电图记录</router-link>
           <router-link to="/aid" class="mdl-navigation__link"><img class="icon" src="../assets/images/icons/aid.svg">一键急救</router-link>
           <router-link to="/user-info" class="mdl-navigation__link"><img class="icon" src="../assets/images/icons/user.svg">个人信息</router-link>
@@ -51,10 +47,13 @@
         </nav>
       </div>
       <main class="mdl-layout__content mdl-color--grey-100">
-        <div class="mdl-grid demo-content">
+      <div class="mdl-spinner mdl-js-spinner is-active loading" v-bind:style="{ display: loadingDisplay }"></div>
+      <!-- <div class="mdl-spinner mdl-js-spinner is-active loading"></div> -->
+      <!-- <div>{{ loadingDisplay }}</div> -->
+        <!-- <div class="mdl-grid demo-content"> -->
           <!-- 下一级视图 -->
           <router-view></router-view>
-        </div>
+        <!-- </div> -->
       </main>
     </div>
   </div>
@@ -62,18 +61,48 @@
 
 <script>
   export default {
+    data () {
+      return {
+        lastElement: null,
+        selectedTitle: '',
+        loadingDisplay: 'none',
+        activate: false
+      }
+    },
     methods: {
       // 当浏览器视口宽度小于1025px时点击侧边栏的链接时抽屉收回
       hide: function (event) {
-        if (window.innerWidth < 1025) {
-          let btn = document.querySelector('.mdl-layout__drawer-button')
-          let target = event.target
-          // console.log(target.tagName)
-          if (target.tagName === 'A') {
+        let target = event.target
+        if (target.tagName === 'A') {
+          if (window.innerWidth < 1025) {
+            let btn = document.querySelector('.mdl-layout__drawer-button')
+            // console.log(target.tagName)
             btn.click()
           }
+          this.selectedTitle = target.innerText
+
+          this.loadingDisplay = 'block'
+
+          // 因为setTimeout中的this指向window，所以得耍个trick，保存当前对象this
+          let that = this
+
+          setTimeout(function () {
+            that.loadingDisplay = 'none'
+          }, 1000)
         } else {
           return
+        }
+      },
+      activate: function (event) {
+        let target = event.target
+        if (target.tagName === 'A') {
+          if (this.lastElement !== null) {
+            this.lastElement.classList.remove('activate')
+          }
+          // this.lastElement.setAttribute('style', '')
+          this.lastElement = event.target
+          // event.target.style = 'background-color: #00BCD4 !important;color: #37474F;'
+          event.target.classList.add('activate')
         }
       }
     }
@@ -81,6 +110,28 @@
 </script>
 
 <style scoped>
+.header-title {
+  font-size:56px;
+  color:white;
+  margin:0 auto;
+}
+.header-font {
+  color: white;
+}
+
+.loading {
+  /*display:none;*/
+  width: 40px;
+  height: 40px;
+  background-color: white;
+  border-radius: 20px;
+  margin: 50px auto;
+}
+
+.activate {
+  background-color: #00BCD4 !important;
+  color: #37474F !important;
+}
 
 label.mdl-button.mdl-js-button.mdl-button--icon {
   top: -20px
