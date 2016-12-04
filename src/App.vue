b673773861ade7878f4f914fdf8718bbd52000f8
Mike Liu <mikelkl@foxmail.com>
2016-12-04 20:41:39 +0800

    add new treatment record

--- src/App.vue
+++ src/App.vue
@@ -2,10 +2,47 @@
   <div id="app">
     <div class="layout mdl-layout mdl-js-layout mdl-layout--fixed-header">
     <!-- <div class="layout mdl-layout mdl-js-layout mdl-layout--fixed-drawer mdl-layout--fixed-header"> -->
-      <my-header v-show="showHeaderDrawer"></my-header>
+      <my-header v-show="showHeaderDrawer" @show="showModal = true"></my-header>
       <my-drawer v-show="showHeaderDrawer"></my-drawer>
       <main class="mdl-layout__content mdl-color--grey-100 my-main">
         <my-loading></my-loading>
+        <my-modal v-if="showModal" @close="showModal = false">
+          <h3 slot="header">添加就诊记录</h3>
+          <form action="#" slot="body">
+            <!-- <div class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label">
+              <input class="mdl-textfield__input" type="text" id="account">
+              <label class="mdl-textfield__label" for="account">用户名</label>
+            </div><br>
+            <div class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label">
+              <input class="mdl-textfield__input" type="password" id="pass">
+              <label class="mdl-textfield__label" for="pass">密码</label>
+            </div> -->
+            <div class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label" v-bind:class="{'is-upgraded': showText1, 'is-focused': showText1}">
+              <input @input="showText1 = true" @focus="showText1 = true" @blur="showText1 = showText1 && record.date" class="mdl-textfield__input" type="text" id="date" v-model="record.date">
+              <label class="mdl-textfield__label" for="date">就诊日期</label>
+            </div><br>
+            <div class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label" v-bind:class="{'is-upgraded': showText2, 'is-focused': showText2}">
+              <input @input="showText2 = true" @focus="showText2 = true" @blur="showText2 = showText2 && record.hospital" class="mdl-textfield__input" type="text" id="hospital" v-model="record.hospital">
+              <label class="mdl-textfield__label" for="hospital">就诊医院</label>
+            </div>
+            <div class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label" v-bind:class="{'is-upgraded': showText3, 'is-focused': showText3}">
+              <input @input="showText3 = true" @focus="showText3 = true" @blur="showText3 = showText3 && record.doctor" class="mdl-textfield__input" type="text" id="doctor" v-model="record.doctor">
+              <label class="mdl-textfield__label" for="doctor">就诊医生</label>
+            </div>
+            <div class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label" v-bind:class="{'is-upgraded': showText4, 'is-focused': showText4}">
+              <input @input="showText4 = true" @focus="showText4 = true" @blur="showText4 = showText4 && record.office" class="mdl-textfield__input" type="text" id="office" v-model="record.office">
+              <label class="mdl-textfield__label" for="office">就诊科室</label>
+            </div>
+            <div class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label" v-bind:class="{'is-upgraded': showText5, 'is-focused': showText5}">
+              <input @input="showText5 = true" @focus="showText5 = true" @blur="showText5 = showText5 && record.diagnose" class="mdl-textfield__input" type="text" id="diagnose" v-model="record.diagnose">
+              <label class="mdl-textfield__label" for="diagnose">病情诊断</label>
+            </div>
+            <button class="mdl-button mdl-js-button mdl-button--raised mdl-button--colored" @click.prevent="">提交</button>
+            <input type="reset" class="mdl-button mdl-js-button mdl-button--raised mdl-button--accent" value="取消" @click.prevent="reset()">
+          </form>
+          <div slot="footer">
+          </div>
+        </my-modal>
         <!-- 下一级视图 -->
         <transition name="component-fade" mode="out-in">
           <router-view></router-view>
@@ -18,21 +55,41 @@
 
 <script>
   import mdl from 'material-design-lite/material.js'
-  import Header from './components/common/Header'
-  import Drawer from './components/common/Drawer'
-  import Loading from './components/utils/Loading'
-  import Tip from './components/utils/Tip'
+
+  const Header = resolve => require(['./components/common/Header'], resolve)
+  const Drawer = resolve => require(['./components/common/Drawer'], resolve)
+  const Loading = resolve => require(['./components/utils/Loading'], resolve)
+  const Tip = resolve => require(['./components/utils/Tip'], resolve)
+  const Modal = resolve => require(['./components/utils/Modal'], resolve)
 
   export default {
     components: {
       'my-header': Header,
       'my-loading': Loading,
       'my-tip': Tip,
-      'my-drawer': Drawer
+      'my-drawer': Drawer,
+      'my-modal': Modal
     },
     computed: {
       showHeaderDrawer: function () {
         return this.$route.name !== 'home' && this.$route.path !== '/login'
+      }
+    },
+    data () {
+      return {
+        record: {},
+        showModal: false,
+        showText1: false,
+        showText2: false,
+        showText3: false,
+        showText4: false,
+        showText5: false
+      }
+    },
+    methods: {
+      reset () {
+        this.record = {}
+        this.showText1 = this.showText2 = this.showText3 = this.showText4 = this.showText5 = false
       }
     }
   }
--- src/components/common/Header.vue
+++ src/components/common/Header.vue
@@ -1,5 +1,6 @@
 <template>
-  <header class="header mdl-layout__header tab-header mdl-color--white">
+  <header class="header mdl-layout__header tab-header mdl-color--white mdl-layout__header--waterfall mdl-layout__header--waterfall-hide-top">
+  <!-- <header class="header mdl-layout__header tab-header mdl-color--white"> -->
     <div class="mdl-layout__header-row mdl-color--white mdl-color-text--grey-600 my-underline">
       <img src="../../assets/images/logo2.png" width="30" height="30" style="margin-right: 5px;">
       <span class="mdl-layout-title">Keeper</span>
@@ -8,11 +9,41 @@
         <label class="mdl-button mdl-js-button mdl-button--icon" for="search">
           <i class="material-icons">search</i>
         </label>
-        <div class="mdl-textfield__expandable-holder">
+        <div class="mdl-textfield__expandable-holder space-right">
           <input class="mdl-textfield__input" type="text" id="search">
           <label class="mdl-textfield__label" for="search">Enter your query...</label>
         </div>
       </div>
+      <i id="notifications" class="material-icons">notifications_none</i>
+      <div class="mdl-tooltip" data-mdl-for="notifications">
+        You have no unread notifications
+      </div>
+      <button id="add-group" class="mdl-button mdl-js-button mdl-button--icon add-group space-right">
+        <i class="material-icons">add</i>
+        <i class="material-icons" style="margin-left:15px;">arrow_drop_down</i>
+      </button>
+      <div class="mdl-tooltip" data-mdl-for="add-group">
+      Create new ...
+      </div>
+      <ul class="mdl-menu mdl-menu--bottom-right mdl-js-menu mdl-js-ripple-effect"
+          for="add-group">
+        <!-- emit event on the App.vue -->
+        <li class="mdl-menu__item" @click="$emit('show')">New record</li>
+      </ul>
+      <img id="s-avatar" v-bind:src="$store.state.userInfo.avatar" class="s-avatar">
+      <div class="mdl-tooltip" data-mdl-for="s-avatar">
+      View profile and more
+      </div>
+      <!-- <ul class="mdl-menu mdl-menu--bottom-right mdl-js-menu"
+          for="s-avatar">
+          <li class="mdl-menu__item--full-bleed-divider">Another Action</li>
+          <li class="avatar-menu-bottom">
+            <button class="mdl-button mdl-js-button mdl-button--raised">
+              setting
+            </button>
+            <button type="button" name="button">2</button>
+          </li>
+      </ul> -->
     </div>
     <!-- Tabs -->
     <div class="mdl-layout__tab-bar mdl-js-ripple-effect my-tab-bar mdl-color--white">
@@ -21,10 +52,37 @@
       <router-link to="/aid" class="mdl-layout__tab mdl-color-text--grey-600">一键急救</router-link>
       <router-link to="/user-info" class="mdl-layout__tab mdl-color-text--grey-600">个人信息</router-link>
     </div>
+
   </header>
 </template>
 
 <style scoped>
+.is-compact {
+  max-height: 49px;
+}
+/*.mdl-menu {
+  padding:0px 0px;
+}
+.avatar-menu-bottom {
+  background: #f5f5f5;
+  width: 100px;
+  padding: 15px;
+}*/
+.add-group {
+  min-width: 52px;
+}
+.space-right {
+  margin-right: 16px;
+}
+.s-avatar {
+  width: 27px;
+  height: 27px;
+  border-radius: 100%;
+  cursor: pointer;
+}
+.s-avatar:focus {
+  outline: none;
+}
 .my-underline {
   border-bottom: 1px solid #e8e8e8;
 }
