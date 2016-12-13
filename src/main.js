d6baf9ed5b00ff694a86b4efd67b575758f959a4
Mike Liu <mikelkl@foxmail.com>
2016-12-13 21:59:11 +0800

    update

--- src/main.js
+++ src/main.js
@@ -5,27 +5,9 @@ import App from './App'
 import router from './router.js'
 import store from './vuex/store.js'
 
-// import Vuex from 'vuex'
-// import VueResource from 'vue-resource'
+import VueResource from 'vue-resource'
 
-// Vue.use(Vuex)
-// Vue.use(VueResource)
-
-// router.beforeEach((to, from, next) => {
-//   if (to.path.indexOf('/main/') !== -1) {
-//     store.dispatch('getCommonStore')
-//     if (store.state.common.isLogin) {
-//       next()
-//     } else {
-//       next('/')
-//     }
-//   } else {
-//     next()
-//   }
-// })
-// router.afterEach(route => {
-//   store.commit('SET_SELECTED_TITLE', route.name)
-// })
+Vue.use(VueResource)
 
 // 4. 创建和挂载根实例。
 // 记得要通过 router 配置参数注入路由，
